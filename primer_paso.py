f0819d8c909d911430957d29ca06f7b9c71ce2df
Alvaro <hernandezsanchezalvarojose@gmail.com>
2026-01-24 21:36:29 +0100

    Integracion de listas JSON para manejar mejor los datos

--- primer_paso.py
+++ primer_paso.py
@@ -1,6 +1,12 @@
 """Mi idea principal de proyecto es hacer juegos de adivinar cosas relacionadas al futbol. Por ejemplo Un 3 en raya con 6 equipos diciendo en cada casilla un jugador que haya jugado en ambos equipos.
     O un woordle (o ahorcado como se dice aqui en españa para adivinar el nombre de un jugador). Mi proyecto estaria basado en la pagina de google: https://futbol-11.com/
     Mi idea seria hacer unos cuantos juegos parecidos a los de esa pagina y añadir algunos creados por mi"""
+import json
+
+def obtenerLista():
+    with open("jugadores.json","r",encoding="utf-8") as file:
+        datos = json.load(file)
+    return datos
 
 
 # En esta funcion lo que hago es pedir al usuario que introduzca los datos dividos por comas para luego con split crear un array y añadirlo al array de jugadores
@@ -15,17 +21,27 @@ def añadirElementos():
     """Comprueba que la edad es un int"""
     try:
         edad = int(edad_str)
-    except ValueError:
-        return "Error: La edad debe ser un número entero."
+    except ValueError:        return "Error: La edad debe ser un número entero."
     
 
-    nombre = datos_lista[0]
-    apellidos = datos_lista[1]
-    nacionalidad = datos_lista[3]
-    equipo = datos_lista[4]
-    nuevo_jugador = [nombre,apellidos,edad,nacionalidad,equipo]
+    nombre = datos_lista[0].strip()
+    apellidos = datos_lista[1].strip()
+    nacionalidad = datos_lista[3].strip()
+    equipo = datos_lista[4].strip()
+    
     if(nombre != "" and apellidos !="" and edad>=15 and nacionalidad !="" and equipo !=""):
-        jugadores.append(nuevo_jugador)
+        nuevo_jugador = {
+            "nombre" : nombre,
+            "apellido": apellidos,
+            "edad" :edad,
+            "pais": nacionalidad,
+            "equipo" : equipo
+        }
+
+        lista = obtenerLista()
+        lista["jugadores"].append(nuevo_jugador)
+        with open("jugadores.json","w",encoding="utf-8") as file:
+            json.dump(lista,file,indent=4,ensure_ascii=False)
         mensaje =(nombre + " " + apellidos + " ha sido añadido correctamente")
     else:
          mensaje = ("Error al añadir. Compruebe los datos")
@@ -33,68 +49,85 @@ def añadirElementos():
 
 # En esta funcion busco los datos completos con el nombre del jugador, recorriendo el array de jugadires y comprobando que el nombre introducido es igual al  primer valor del array
 def buscarElemento():
-    nombre = input("Introduce le nombre del jugador: ")
+    nombre = input("Introduce el nombre del jugador: ")
     nombreLimpio = nombre.strip()
+    lista = obtenerLista()
+    jugadores = lista["jugadores"]
 
     for jugador in jugadores:
-        nombreJugador = jugador[0].strip()
-
-        if(nombreLimpio == nombreJugador):
-            return jugador
+        if(nombreLimpio.lower() == jugador["nombre"].lower()):
+            return jugador["nombre"] + " " + jugador["apellido"] + " ,edad:" + str(jugador["edad"]) + " ,nacionalidad:" + jugador["pais"] + " ,equipo actual:" + jugador["equipo"] + "\n"
         
     return None
 
 # Para poder modificarlo he utilizado el metodo enumerate, para poder enumerar cada array y poder llevar la cuenta de cuantas veces se ha iterado y asi con el indicie i poder modificarlo
 def modificarElemento():
-    nombre = input("Introduce le nombre del jugador: ")
+    nombre = input("Introduce el nombre del jugador: ")
     nombreLimpio = nombre.strip()
+    lista_json = obtenerLista()
+    jugadores = lista_json["jugadores"]
+
     for i,jugador in enumerate(jugadores):
-        nombreJugador = jugador[0].strip()
-        if(nombreLimpio == nombreJugador):
+        if(nombreLimpio.lower() == jugador["nombre"].lower()):
+            """Pedir nuevos datos"""
             datos = input("Introduce los nuevos valores del jugador (Nombre, apellidos, edad, pais, equipo) Separalo todo con , por favor ")
             datos_lista = datos.split(",")
+
             if len(datos_lista) !=5:
                 print("Error debes introducir exacatamente 5 valores")
                 return
             
-            edad_str = datos_lista[2]
+            edad_str = datos_lista[2].strip()
             """Comprueba que la edad es un int"""
             try:
                 edad = int(edad_str)
             except ValueError:
                 return "Error: La edad debe ser un número entero."
-            nombre = datos_lista[0]
-            apellidos = datos_lista[1]
+            nombre = datos_lista[0].strip()
+            apellidos = datos_lista[1].strip()
+            nacionalidad = datos_lista[3].strip()
+            equipo = datos_lista[4].strip()
             
-            nacionalidad = datos_lista[3]
-            equipo = datos_lista[4]
-            nuevo_jugador = [nombre,apellidos,edad,nacionalidad,equipo]
             if(nombre != "" and apellidos !="" and edad>=15 and nacionalidad !="" and equipo !=""):
-                jugadores[i] = nuevo_jugador
-                mensaje =(nombre + " " + apellidos + " ha sido modificado correctamente")
+                jugadores[i] ={
+                    "nombre": nombre,
+                    "apellido": apellidos,
+                    "edad": edad,
+                    "pais": nacionalidad,
+                    "equipo": equipo
+                }
+                with open("jugadores.json","w",encoding="utf-8") as file:
+                    json.dump(lista_json,file,indent=4,ensure_ascii=False)
+                return (nombre + " " + apellidos + " ha sido modificado correctamente")
             else:
-                mensaje = ("Error al añadir. Compruebe los datos")
-            return mensaje
+                return ("Error al añadir. Compruebe los datos")
+    return "Jugador no encontrado"
 
 # Y para esta funcion he hecho la misma jugada que modificar, solo que esta vez en vez de modificar, elimino el array
 def eliminarElemento():
-    nombre = input("Introduce le nombre del jugador que quieras eliminar: ")
+    nombre = input("Introduce el nombre del jugador que quieras eliminar: ")
     nombreLimpio = nombre.strip()
+    lista_json = obtenerLista()
+    jugadores = lista_json["jugadores"]
     for i,jugador in enumerate(jugadores):
-        nombreJugador = jugador[0].strip()
-        apellidoJugador = jugador[1].strip()
-        if(nombreLimpio == nombreJugador):
+        if(nombreLimpio.lower() == jugador["nombre"].lower()):
+            nombreJugador = jugador["nombre"]
+            apellidoJugador = jugador["apellido"]
             del jugadores[i]
+            with open("jugadores.json", "w", encoding="utf-8") as file:
+                json.dump(lista_json, file, indent=4, ensure_ascii=False)
             mensaje = nombreJugador + " " + apellidoJugador + " eliminado correctamente"
-        else:
-            mensaje= "Jugador no encontrado"
+            return mensaje
+    mensaje= "Jugador no encontrado"
     return mensaje
 
-# Ene sta funcion recorro el array y lo almaceno en una variable para poder mostrar todos los datos
+# En esta funcion recorro el array y lo almaceno en una variable para poder mostrar todos los datos
 def mostrarTodos():
+    lista_json = obtenerLista()
+    jugadores = lista_json["jugadores"]
     total = ""
     for jugador in jugadores:
-        total += jugador[0] + " " + jugador[1] + " ,edad:" + str(jugador[2]) + " ,nacionalidad:" + jugador[3] + " ,equipo actual:" + jugador[4] + "\n"
+        total += jugador["nombre"] + " " + jugador["apellido"] + " ,edad:" + str(jugador["edad"]) + " ,nacionalidad:" + jugador["pais"] + " ,equipo actual:" + jugador["equipo"] + "\n"
     return total
 
 
@@ -114,12 +147,7 @@ def menu():
         elif(entrada == 2):
             resultado = buscarElemento()
             if resultado:
-                nombre = resultado[0]
-                apellidos = resultado[1]
-                edad = resultado[2]
-                nacionalidad = resultado[3]
-                equipo = resultado[4]
-                print(nombre + " " + apellidos + " " + str(edad) + " "+ " " + nacionalidad + " " + equipo)
+                print(resultado)
             else:
                 print("No hemos podido encontrar al jugador")
         elif (entrada ==3):
