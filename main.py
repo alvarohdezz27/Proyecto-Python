f0d98e8fbcc12ca656fa449ddc4696dbbe040120
Alvaro <hernandezsanchezalvarojose@gmail.com>
2026-02-13 21:18:07 +0100

    Finalizacion definitiva de proyecto

--- main.py
+++ main.py
@@ -19,7 +19,14 @@ def menu():
     logging.info("Aplicación iniciada")
     while True:
         try:
-            entrada = int(input("¿Que deseas hacer? 1-Añadir elemento, 2-Buscar elemento, 3-Modificar elemento, 4-Eliminar elemento, 5-Mostrar todos,6-Salir "))
+            entrada = int(input("¿Que deseas hacer? " \
+            "1-Añadir elemento, " \
+            "2-Buscar elemento, " \
+            "3-Modificar elemento, " \
+            "4-Eliminar elemento, " \
+            "5-Mostrar todos, " \
+            "6-Generar un reporte, " \
+            "7-Salir "))
         except ValueError:
             logging.warning("Entrada no válida en el menú")
             print("Entrada no válida. Por favor, introduce un número.")
@@ -85,81 +92,15 @@ def menu():
                 resultado = Entrenador.mostrar_elementos()
                 print("--TOTAL DE ENTRENADORES--")
                 print(resultado)
+        elif(entrada ==6):
+            resultado = generar_reporte()
+            print(resultado)
         else:
             logging.info("Aplicación cerrada por el usuario")
             print("Hasta pronto!")
             break
 
 
-        # if(eleccion==1):
-        #     try:
-        #         entrada = int(input("¿Que deseas hacer? 1-Añadir elemento, 2-Buscar elemento, 3-Modificar elemento, 4-Eliminar elemento, 5-Mostrar todos,6-Salir "))
-        #     except ValueError:
-        #         logging.warning("Entrada no válida en el menú")
-        #         print("Entrada no válida. Por favor, introduce un número.")
-        #         continue
-        #     if(entrada ==1):
-        #         respuesta = Jugador.insertar_elemento()
-        #         print(respuesta)
-        #     elif(entrada ==2):
-        #         respuesta = Jugador.buscar_elemento()
-        #         if respuesta:
-        #             print(respuesta.mostrarjugador())
-        #     elif(entrada ==3):
-        #         respuesta = Jugador.modificar_elemento()
-        #         print(respuesta)
-        #     elif(entrada==4):
-        #         respuesta = Jugador.eliminar_elemento()
-        #         print(respuesta)
-        #     elif(entrada==5):
-        #         respuesta = Jugador.mostrar_elementos()
-        #         print("--TOTAL DE JUGADORES--")
-        #         print(respuesta)
-        #     else:
-        #         logging.info("Aplicación cerrada por el usuario")
-        #         print("Hasta pronto!")
-        #         break
-
-        # elif(eleccion ==2):
-        #     try:
-        #         entrada = int(input("¿Que deseas hacer? 1-Añadir elemento, 2-Buscar elemento, 3-Modificar elemento, 4-Eliminar elemento, 5-Mostrar todos,6-Salir "))
-        #     except ValueError:
-        #         logging.warning("Entrada no válida en el menú")
-        #         print("Entrada no válida. Por favor, introduce un número.")
-        #         continue
-        #     if(entrada ==1):
-        #         respuesta = Entrenador.insertar_elemento()
-        #         print(respuesta)
-        #     elif (entrada ==2):
-        #         respuesta = Entrenador.buscarEntrenador()
-        #         if(respuesta):
-        #             print(respuesta.mostrarEntrenador())
-        #         else:
-        #             print("ERROR")
-        #     elif (entrada ==3):
-        #         respuesta = Entrenador.modificarEntrenador()
-        #         print(respuesta)
-        #     elif (entrada ==4):
-        #         respuesta = Entrenador.eliminarEntrenador()
-        #         print(respuesta)
-        #     elif (entrada == 5):
-        #         respuesta = Entrenador.mostrarEntrenadores()
-        #         print("--TOTAL DE ENTRENADORES--")
-        #         print(respuesta)
-        #     else:
-        #         logging.info("Aplicación cerrada por el usuario")
-        #         print("Hasta pronto!")
-        #         break
-        # elif(eleccion ==3):
-        #     try:
-        #         entrada = int(input("¿Que deseas hacer? 1-Añadir elemento, 2-Buscar elemento, 3-Modificar elemento, 4-Eliminar elemento, 5-Mostrar todos,6-Salir "))
-        #     except ValueError:
-        #         logging.warning("Entrada no válida en el menú")
-        #         print("Entrada no válida. Por favor, introduce un número.")
-        #         continue
-        #     if(entrada ==1):
-        #         respuesta = Arbitro.insertar_elemento()
-        #         print(respuesta)
 
 #Funcion para obtener el json
 def obtenerLista():
